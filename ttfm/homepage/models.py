6dafa65ce839484fc81e65d5717edf98c4d22d33
renicao <reni.cao@gmail.com>
2016-01-23 07:06:32 +0000

    Please enter the commit message for your changes. Lines starting

--- ttfm/homepage/models.py
+++ ttfm/homepage/models.py
@@ -2,4 +2,10 @@ from __future__ import unicode_literals
 
 from django.db import models
 
-# Create your models here.
+
+class Messages(models.Model):
+    message_id = models.CharField(max_length=255)
+    receive_date = models.DateTimeField('date message received')
+    set_date = models.DateTimeField('date scheduled for delivery')
+    message_body = models.CharField(max_length=255)
+    message_email = models.CharField(max_length=255)
